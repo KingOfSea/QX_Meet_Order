5521b66bd1c1aa5506b71667991bb49b52e7f46a
wangzhihai <wangzhihai@didiglobal.com>
2022-10-14 13:05:34 +0800

    up

--- qx_meet_info.js
+++ qx_meet_info.js
@@ -2,10 +2,10 @@ const meet_ck = "test"
 const meet_date = "20221010"
 $prefs.setValueForKey(meet_ck, 'meet_ck')
 $prefs.setValueForKey(meet_date, 'meet_date')
-// $.info(`meet_ck = ${meet_ck}`)
-// $.info(`meet_date = ${meet_date}`)
+console.log(`meet_ck = ${meet_ck}`)
+console.log(`meet_date = ${meet_date}`)
 
 var name = $prefs.valueForKey('meet_ck')
-$.info(`meetkey = ${name}`)
+console.log(`meetkey = ${name}`)
 
 $.done();
