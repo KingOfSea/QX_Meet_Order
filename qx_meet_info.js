3645a180f5843750348774e5f6ffff9d97c20865
wangzhihai <wangzhihai@didiglobal.com>
2022-10-14 11:19:31 +0800

    up

---
+++ qx_meet_info.js
@@ -0,0 +1,7 @@
+const meet_ck = ""
+const meet_date = ""
+$prefs.setValueForKey(meet_ck, "meet_ck")
+$prefs.setValueForKey(meet_date, "meet_date")
+$.info(`meet_ck = ${meet_ck}`)
+$.info(`meet_date = ${meet_date}`)
+$.done();
