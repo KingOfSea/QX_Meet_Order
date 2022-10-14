cd8843694fb5c21b988e4fcc59694592d009c8fe
wangzhihai <wangzhihai@didiglobal.com>
2022-10-14 14:49:25 +0800

    up

--- qx_meet_info.js
+++ qx_meet_info.js
@@ -1,5 +1,5 @@
 const meet_ck = "test"
-const meet_date = "20221010"
+const meet_date = "2022-10-10"
 $prefs.setValueForKey(meet_ck, 'meet_ck')
 $prefs.setValueForKey(meet_date, 'meet_date')
 console.log(`meet_ck = ${meet_ck}`)
